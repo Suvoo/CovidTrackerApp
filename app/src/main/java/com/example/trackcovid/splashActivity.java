21a9bc6c6ba4dad0485619dd2552067237ee96a9
Suvoo <suvodeepsinha1981@gmail.com>
2020-04-24 14:11:07 +0530

    updated spalsh screen and ui changes

---
+++ app/src/main/java/com/example/trackcovid/splashActivity.java
@@ -0,0 +1,42 @@
+package com.example.trackcovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class splashActivity extends AppCompatActivity {
+    private TextView tv;
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        tv = (TextView)findViewById(R.id.tv);
+        iv = (ImageView)findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+        final Intent i = new Intent(splashActivity.this,MainActivity.class);
+        Thread timer =  new Thread(){
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+
+                }
+            }
+        }; timer.start();
+    }
+}
+
